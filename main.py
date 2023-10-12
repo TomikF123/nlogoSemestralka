5a640879a1a71f5b0620754f31edbc50a5c5900b
TomikF123 <tomas.felcan@icloud.com>
2023-10-12 14:29:54 +0200

    restructured and step counts acounted for

--- main.py
+++ main.py
@@ -4,22 +4,19 @@ from matplotlib import pyplot as plt
 
 file = "/home/tomas/school/ai/semestralka/analysis/BB_3k.csv"
 
-#Prislo mi to nejlepsi udelat na format "[run number]","[step]","initial-density","count patches with [pcolor = white]","count patches with [pcolor = red]"
-# tzn jeden radek jedna hotova simulace o jednom n tickach ale hodnoty pouze z posledniho ticku.
-# takze predmetem je zkoumat jak se to podle initial dens. prumerne chova v ntym ticku u 300k simulacich je graf smooth xd
-
-
 
 df = pd.read_csv(file)
-df.rename(columns={"count patches with [pcolor = white]":"n_alive","count patches with [pcolor = red]":"n_dying"},inplace=True)
+df.rename(columns={"count patches with [pcolor = white]":"n_alive","count patches with [pcolor = red]":"n_dying","[step]":"step"},inplace=True)
 print(df)
-df = df[["initial-density", "n_alive", "n_dying"]]
+
+# Selecting only the columns of interest
+df = df[["initial-density", "n_alive", "n_dying","step"]]
 
 df_mean = df.groupby(
-    by="initial-density")[["n_alive", "n_dying"]].mean().reset_index()
-print(df_mean)
+    by="initial-density")[["n_alive", "n_dying","step"]].mean().reset_index()
 
-# ration between 
+
+# Calculating some ratios, may be included in the report 
 ratio = []
 
 for ix in df_mean.index:
@@ -29,13 +26,17 @@ for ix in df_mean.index:
     ratio.append(round(alive/dying,ndigits=3))
 df_mean["ratio"] = ratio
 
+#Setting the Figure to i by j subplots, creating common values for X axis
 fig, ax = plt.subplots(2, 2)
+X = df_mean.index
 
-X_ratio = df_mean.index
-Y_ratio = df_mean["ratio"]
-graph_ratio = ax[0][0].plot(X_ratio, Y_ratio)
+# Plot 1 - avg Step until equlibrium ?
+Y_step = df_mean["step"]
+graph_step = ax[0][0].plot(X, Y_ratio)
 
-X_mean_alive = df_mean.index
+#plot 2 ? 
+""" X_mean_alive = df_mean.index
 Y_mean_alive = df_mean["n_alive"]
-graph_ratio = ax[0][1].plot(X_mean_alive, Y_mean_alive)
+graph_ratio = ax[0][1].plot(X_mean_alive, Y_mean_alive) """
+
 plt.show()
